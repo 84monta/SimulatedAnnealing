9eab19726f74a2f4d033202b85d62e32902b06ae
Monta Yashi <84monta@gmail.com>
2017-12-10 08:45:03 +0900

    added cupy code
    To enable cupy
    GPU = True
    To disable cupy
    GPU = False

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -3,66 +3,99 @@ import json
 import math
 import time
 
-#Definition 
-T_MAX  = 800.0
-T_MIN  = 1.0
-MAX_STEMPS = 100
+import cupy as cp
+import chainer.cuda
 
-T_LOOP = 300
-PERSONS = 100
+#Definition 
+T_MAX  = 100.0
+T_MIN  = 0.001
+MAX_STEMPS = 20
+
+T_LOOP = 600
+PERSONS = 1000
+
+GPU = True
+
+TRIAL=1
+
 
 #Load Edge Data into narray
 #Change file name
-f = open('201712030215556824-Jij.json', 'r')
-jsonData = json.load(f)
-edge_array = np.array(jsonData)
+#f = open('201712030215556824-Jij.json', 'r') # for 100 CHANGE PERSON =100
+f = open('201711282214561557-Jij.json', 'r') # for 1000 CHANGE PERSON =1000
+#f = open('201712052235195895-Jij.json', 'r') # for 2000 CHANGE PERSON =2000
 
-#Create temporary 
-person = np.random.choice(2, PERSONS)*2-1
-tri_array = np.tri(PERSONS) - np.eye(PERSONS)
+jsonData = json.load(f)
+if GPU :
+    edge_array = chainer.cuda.to_gpu(np.array(jsonData))
+    tri_array = chainer.cuda.to_gpu(np.tri(PERSONS) - np.eye(PERSONS))
+else:
+    edge_array = np.array(jsonData)
+    tri_array = np.tri(PERSONS) - np.eye(PERSONS)
+
+
 
 ## calculate energy
 def ret_energy(Edge,Person):
-    return np.sum(Edge * (tri_array - np.outer(Person, Person) * tri_array)/2.0)
-
+    if GPU :
+        return cp.sum(Edge * tri_array * (1.0 - cp.outer(Person, Person))/2.0)
+    else:
+        return np.sum(Edge * tri_array * (1.0 - np.outer(Person, Person))/2.0)
+    
 ## return move new status or not
 def ret_prob(PreE,CurE,temp):
-    if PreE < CurE :
-        if math.exp((PreE-CurE)/temp) < np.random.rand():
-            return False
-    return True
-
-#Initialization
-steps = 1
-current_temp = T_MAX
-current_energy = ret_energy(edge_array,person)
-
-#Create new status area
-tmp_person = person.copy()
-
-start_time = time.time()
-#main loop
-while steps < MAX_STEMPS:
-    steps = steps + 1
+    if GPU :
+        if PreE < CurE :
+            if math.exp((PreE-CurE)/temp) < cp.random.rand():
+                return False
+        return True
+    else:
+        if PreE < CurE :
+            if math.exp((PreE-CurE)/temp) < np.random.rand():
+                return False
+        return True
     
-    #print current temperature and energy
-    print("current temperature = %4.4f , current energy = %f" % (current_temp,current_energy))
+for i in range(TRIAL):
+
+    cp.random.seed(i)
     
-    #iteration on same temperature
-    i = 1
-    while i < T_LOOP:
-        i = i + 1
-        selecter = np.random.randint(PERSONS)
-        tmp_person[selecter] = -tmp_person[selecter] 
-        tmp_energy = ret_energy(edge_array,tmp_person)
-        if ret_prob(current_energy,tmp_energy,current_temp):
-            current_energy = tmp_energy
-            person[selecter] = -person[selecter]
-        else:
+    #Initialization
+    steps = 1
+    current_temp = T_MAX
+    if GPU :
+        person = cp.random.choice(2, PERSONS)*2-1
+    else:
+        person = np.random.choice(2, PERSONS)*2-1
+        
+    current_energy = ret_energy(edge_array,person)
+    
+    
+    #Create new status area
+    tmp_person = person.copy()
+    start_time = time.time()
+    #main loop
+    while steps < MAX_STEMPS:
+        steps = steps + 1
+        
+        #print current temperature and energy
+        print("current temperature = %6.6f , current energy = %f" % (current_temp,current_energy))
+        
+        #iteration on same temperature
+        i = 1
+        while i < T_LOOP:
+            i = i + 1
+            selecter = np.random.randint(PERSONS)
             tmp_person[selecter] = -tmp_person[selecter] 
-    current_temp = T_MAX * math.exp(-math.log(T_MAX / T_MIN)* steps/MAX_STEMPS)
-  
-end_time = time.time()  
-print("elapsed_time:{0}".format(end_time - start_time) + "[sec]")
-print("**Final temperature = %4.4f , current energy = %f**" % (current_temp,current_energy))
+            tmp_energy = ret_energy(edge_array,tmp_person)
+            if ret_prob(current_energy,tmp_energy,current_temp):
+                current_energy = tmp_energy
+                person[selecter] = -person[selecter]
+            else:
+                tmp_person[selecter] = -tmp_person[selecter] 
+        current_temp = T_MAX * math.exp(-math.log(T_MAX / T_MIN)* steps/MAX_STEMPS)
+      
+    end_time = time.time() 
+     
+    print("elapsed_time:{0}".format(end_time - start_time) + "[sec]")
+    print("**Final temperature = %6.6f , current energy = %f**" % (current_temp,current_energy))
 
